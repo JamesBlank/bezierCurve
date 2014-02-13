dd36fa697515f6ffe544302cb731cc11127154b0
Aliquis <y.kalinichenko@crystalteam.ru>
2014-02-13 16:24:16 +0400

    all arrow directions are available

--- script.js
+++ script.js
@@ -30,7 +30,6 @@ function curveBezier(x1, y1, x2, y2, optionsUser) {
         canvas.style.top      = top+'px';
         canvas.style.left     = left+'px';
         canvas.style.zIndex   = 2;
-    document.body.appendChild(canvas);
 
     //replacing default options with user settings
     for (var key in optionsUser) {
@@ -54,15 +53,10 @@ function curveBezier(x1, y1, x2, y2, optionsUser) {
     }
 
     //direction of curve drawing
-    var dir= -Math.abs(x2-x1)/(x2-x1);
-    if (dir == 1) {
-        var point_0 = new point(canvas.width,canvas.height);
-        var point_2 = new point(offset, offset);
-    }
-    else if (dir == -1) {
-        var point_0 = new point(0,0);
-        var point_2 = new point(canvas.width-offset, canvas.height-offset);
-    }
+    var dirY = Math.abs(y2-y1)/(y2-y1) < 0 ? 0 : 1;
+    var dirX = Math.abs(x2-x1)/(x2-x1) < 0 ? 0 : 1;
+    var point_0 = new point(Math.abs(x1-top-dirX*offset),Math.abs(y1-left-dirY*offset));
+    var point_2 = new point(Math.abs(x2-top-dirX*offset),Math.abs(y2-left-dirY*offset));
 
     var Bx_prev = point_0.x;
     var By_prev = point_0.y;
@@ -72,10 +66,11 @@ function curveBezier(x1, y1, x2, y2, optionsUser) {
     var i=0;                  //iteration counter
 
     //curve camber
+    var camberDegree = 0.2;
     if (options.camber == '+')
-        var point_1 = new point(canvas.width*0.2, canvas.height*0.8);
+        var point_1 = new point(canvas.width*Math.abs(dirX-(1-camberDegree)), canvas.height*Math.abs(dirY-camberDegree));
     else if (options.camber == '-')
-        var point_1 = new point(canvas.width*0.8, canvas.height*0.2);
+        var point_1 = new point(canvas.width*Math.abs(dirX-camberDegree), canvas.height*Math.abs(dirY-(1-camberDegree)));
 
 
     var interval = setInterval(draw, options.speed/stepNum);
@@ -105,18 +100,21 @@ function curveBezier(x1, y1, x2, y2, optionsUser) {
             bezier.strokeStyle = options.color;
             clearInterval(interval);
 
-            var alpha = Math.atan((By-By_prev)/(Bx-Bx_prev));
+            var alpha = Math.atan((By_prev-By)/(Bx_prev-Bx));
 
-            var arrowX_1 = point_2.x + dir*options.arrow['length']*Math.cos(alpha-options.arrow['angle']/2);
-            var arrowY_1 = point_2.y + dir*options.arrow['length']*Math.sin(alpha-options.arrow['angle']/2);
+            dirX = (dirX == 0) ? -1 : 1;
+            dirY = (dirY == 0) ? -1 : 1;
+
+            var arrowX_1 = point_2.x - dirX*options.arrow['length']*Math.cos(alpha-options.arrow['angle']/2);
+            var arrowY_1 = point_2.y - dirX*options.arrow['length']*Math.sin(alpha-options.arrow['angle']/2);
             bezier.beginPath();
             bezier.moveTo(Bx, By);
             bezier.lineTo(arrowX_1, arrowY_1);
             bezier.stroke();
             bezier.closePath();
 
-            var arrowX_2 = point_2.x + dir*options.arrow['length']*Math.cos(alpha+options.arrow['angle']/2);
-            var arrowY_2 = point_2.y + dir*options.arrow['length']*Math.sin(alpha+options.arrow['angle']/2);
+            var arrowX_2 = point_2.x - dirX*options.arrow['length']*Math.cos(alpha+options.arrow['angle']/2);
+            var arrowY_2 = point_2.y - dirX*options.arrow['length']*Math.sin(alpha+options.arrow['angle']/2);
             bezier.beginPath();
             bezier.moveTo(Bx, By);
             bezier.lineTo(arrowX_2, arrowY_2);
@@ -128,8 +126,6 @@ function curveBezier(x1, y1, x2, y2, optionsUser) {
         By_prev = By;
     }
 
-    this.delete = function() {
-        document.body.removeChild(canvas);
-    };
+    return canvas;
 
 }
